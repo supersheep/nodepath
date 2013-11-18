6b3c4915c15ec242e3aa0ba7ec558800f2261dd0
supersheep <supersheepxjy@gmail.com>
2013-11-18 13:46:43 +0800

    change api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var exec = require("child_process").exec;
 
 
-module.exports = function(callback){
+exports.get = function(callback){
 	exec("npm prefix -g",function(err,stdout,stderr){
 		callback && callback(stdout.trim());
 	});
